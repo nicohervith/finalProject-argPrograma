30bb8d3cb0b661b6e4ab391a002ac740faa25993
nicohervith <niko_hervith@hotmail.com>
2022-12-22 15:34:22 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialComponent,
+    BannerComponent,
+    AboutComponent,
+    ProjectsComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule
